10ccb60d892c4ca0b2b5070c1df2cda4f3b0ee17
CaffeineDuck <hello@samrid.me>
2024-08-05 13:04:38 +0545

    feat: add countup for reach-stats

--- src/components/home/Reach.tsx
+++ src/components/home/Reach.tsx
@@ -4,23 +4,24 @@ import eventsSvg from "../../assets/events.svg";
 import { useEffect, useState } from "react";
 import { getDoc, doc } from "firebase/firestore";
 import db from "../../lib/firebase";
-
-interface ReachStats {
-  districtsReached: string; // Keeping these as string since they might have `+` sign
-  eventsDone: string;
-  helpedChildrenCount: string;
-}
+import CountUp from "react-countup";
+import useInView from "../../hooks/useInView";
 
 interface MetaData {
-  home_page_stats: ReachStats;
+  home_page_stats: {
+    districtsReached: string;
+    eventsDone: string;
+    helpedChildrenCount: string;
+  };
 }
 
 function Reach() {
-  const [metaData, setMetaData] = useState<MetaData>();
+  const [metaData, setMetaData] = useState<MetaData | null>(null);
+  const [ref, isInView] = useInView({ threshold: 0.1 });
 
   const fetchMeta = async () => {
     try {
-      const docRef = doc(db, "meta", "eY3YESNTBiBrZdJFpG10"); // First document in the Meta collection
+      const docRef = doc(db, "meta", "eY3YESNTBiBrZdJFpG10");
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
@@ -33,24 +34,24 @@ function Reach() {
       console.error("Error fetching meta document: ", error);
     }
   };
+
   useEffect(() => {
     fetchMeta();
   }, []);
 
-  let reachStats = metaData?.home_page_stats;
-  if (!metaData) {
-    reachStats = {
-      districtsReached: "loading..",
-      eventsDone: "loading..",
-      helpedChildrenCount: "loading..",
-    };
-    setMetaData({ home_page_stats: reachStats });
-  }
+  const reachStats = metaData?.home_page_stats || {
+    districtsReached: "0",
+    eventsDone: "0",
+    helpedChildrenCount: "0",
+  };
 
   return (
     <section id="reach" className="mt-20 px-2 md:px-0">
-      <div className="p-6 md:p-12 bg-primary rounded-2xl container mx-auto flex flex-col just items-center space-y-10 md:flex-row md:space-x-16 md:space-y-0">
-        {/* <!-- Reach Info --> */}
+      <div
+        ref={ref}
+        className="p-6 md:p-12 bg-primary rounded-2xl container mx-auto flex flex-col just items-center space-y-10 md:flex-row md:space-x-16 md:space-y-0"
+      >
+        {/* Reach Info */}
         <div className="text-center md:w-1/3 md:text-start md:items-start space-y-3">
           <h2 className="text-3xl font-bold text-white">Our Reach</h2>
           <p className="text-sm text-white">
@@ -60,12 +61,20 @@ function Reach() {
           </p>
         </div>
 
-        {/* <!-- React Catalog --> */}
+        {/* React Catalog */}
         <div className="flex items-between space-y-10 flex-col w-full md:flex-row md:items-start md:w-2/3 md:space-y-0 justify-between md:space-x-10">
-          {/* <!-- Reach 1 --> */}
+          {/* Reach 1 */}
           <div className="md:w-1/3 items-center text-center space-y-3 md:space-y-5">
             <h2 className="text-5xl font-bold text-white">
-              {reachStats?.districtsReached}
+              {isInView && (
+                <>
+                  <CountUp
+                    end={parseInt(reachStats.districtsReached)}
+                    duration={2.5}
+                  />
+                  {"+"}
+                </>
+              )}
             </h2>
             <div className="flex items-center justify-center space-x-3">
               <img className="w-[30px] h-[30px]" src={placesSvg} alt="places" />
@@ -73,10 +82,18 @@ function Reach() {
             </div>
           </div>
 
-          {/* <!-- Reach 2 --> */}
+          {/* Reach 2 */}
           <div className="md:w-1/3 items-center text-center space-y-3 md:space-y-5">
             <h2 className="text-5xl font-bold text-white">
-              {reachStats?.helpedChildrenCount}
+              {isInView && (
+                <>
+                  <CountUp
+                    end={parseInt(reachStats.helpedChildrenCount)}
+                    duration={2.5}
+                  />
+                  {"+"}
+                </>
+              )}
             </h2>
             <div className="flex items-center justify-center space-x-3">
               <img
@@ -88,10 +105,18 @@ function Reach() {
             </div>
           </div>
 
-          {/* <!-- Reach 3 --> */}
+          {/* Reach 3 */}
           <div className="md:w-1/3 items-center text-center space-y-3 md:space-y-5">
             <h2 className="text-5xl font-bold text-white">
-              {reachStats?.eventsDone}
+              {isInView && (
+                <>
+                  <CountUp
+                    end={parseInt(reachStats.eventsDone)}
+                    duration={2.5}
+                  />
+                  {"+"}
+                </>
+              )}
             </h2>
             <div className="flex items-center justify-center space-x-3">
               <img className="w-[30px] h-[30px]" src={eventsSvg} alt="places" />
